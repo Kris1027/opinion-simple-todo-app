27d61465382839e2a4e2afdef238587aa8d4c124
Kris1027 <obarzanek.work@gmail.com>
2024-12-02 22:27:56 +0100

    update today date with day of the week

--- src/components/today-date/today-date.jsx
+++ src/components/today-date/today-date.jsx
@@ -2,10 +2,17 @@ import styles from './today-date.module.css';
 
 const TodayDate = () => {
     const now = new Date();
+    const weekday = now.toLocaleDateString('en-US', { weekday: 'long' });
     return (
         <div className={styles.todayDate}>
-            <span>Today</span>
-            <span>{now.toLocaleDateString()}</span>
+            <span>Today is {weekday}</span>
+            <span>
+                {now.toLocaleDateString('pl-PL', {
+                    day: '2-digit',
+                    month: '2-digit',
+                    year: 'numeric',
+                })}
+            </span>
         </div>
     );
 };
